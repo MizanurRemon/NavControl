c7958e0abd71f3d5fb1992953e92fe75c78021d0
= <=>
2023-04-02 19:53:18 +0600

    safdf

--- app/src/main/java/com/example/navcontrol/Screen/DataScreen.kt
+++ app/src/main/java/com/example/navcontrol/Screen/DataScreen.kt
@@ -32,7 +32,7 @@ fun DataScreen(onBack: () -> Unit, message: String?) {
                 }
 
                 Text(
-                    text = "Test",
+                    text = "Hello",
                     style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold)
                 )
             }
