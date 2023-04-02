37c358ac033cb3f0975070cdc02178573183049b
= <=>
2023-04-02 19:56:35 +0600

    rename

--- app/src/main/java/com/example/navcontrol/Screen/DataScreen.kt
+++ app/src/main/java/com/example/navcontrol/Screen/DataScreen.kt
@@ -32,7 +32,7 @@ fun DataScreen(onBack: () -> Unit, message: String?) {
                 }
 
                 Text(
-                    text = "NAV",
+                    text = "PPP",
                     style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold)
                 )
             }
